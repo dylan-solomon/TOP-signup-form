a1e2375aa32ad1e5b35cc55328e1d095a713a3dd
Dylan Solomon <dylansolomon413@gmail.com>
2022-04-13 17:46:54 -0700

    Add error class

--- script.js
+++ script.js
@@ -1,6 +1,39 @@
-const pass = document.getElementById('pass');
-const cpass = document.getElementById('cpass');
+const pass = document.getElementById("pass");
+const cpass = document.getElementById("cpass");
 
-pass.addEventListener('keypress', () => {
+const passwords = document.getElementsByClassName('.password');
 
+pass.addEventListener("keyup", () => {
+    console.log('hi');
+    if (pass.value != cpass.value) {
+        console.log('no')
+        pass.classList.add('match-error');
+        cpass.classList.add('match-error')
+    }
+})
+
+cpass.addEventListener("keyup", () => {
+    console.log('hi');
+    if (pass.value != cpass.value) {
+        console.log('no')
+        pass.classList.add('match-error');
+        cpass.classList.add('match-error')
+    }
+})
+
+pass.addEventListener("keyup", () => {
+
+    if (pass.value == cpass.value) {
+        console.log('yes')
+        pass.classList.remove('match-error');
+        cpass.classList.remove('match-error')
+    }
+})
+
+cpass.addEventListener("keyup", () => {
+    if (pass.value == cpass.value) {
+        console.log('yes')
+        pass.classList.remove('match-error');
+        cpass.classList.remove('match-error')
+    }
 })
